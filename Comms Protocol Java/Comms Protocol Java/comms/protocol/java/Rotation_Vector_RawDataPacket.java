7a28fc64c7551d460409528e8418906cf075c607
shastaclokey1 <shasta.clokey@vmuv.io>
2018-03-28 14:35:14 -0700

    added a quaternion class to store the float array data that comes from the rotation vector data packets added byte/float serialize funcitons and implemeted unit tests for java data protocol

--- Comms Protocol Java/Comms Protocol Java/comms/protocol/java/Rotation_Vector_RawDataPacket.java
+++ Comms Protocol Java/Comms Protocol Java/comms/protocol/java/Rotation_Vector_RawDataPacket.java
@@ -7,7 +7,38 @@ public class Rotation_Vector_RawDataPacket extends DataPacket
 	
 	public Rotation_Vector_RawDataPacket()
     {
-		super(ValidPacketTypes.rotation_vector_raw_data_packet, (short)16, new byte[0]);
+		super(ValidPacketTypes.rotation_vector_raw_data_packet, (short)16, new byte[16]);
+    }
+	
+	public Rotation_Vector_RawDataPacket(byte[] payload) throws Exception
+    {
+		super(ValidPacketTypes.rotation_vector_raw_data_packet, (short)16, new byte[16]);
+        Serialize(payload);
+    }
+	
+	public Rotation_Vector_RawDataPacket(DataPacket packet) throws Exception
+    {
+        if ((packet.getPacketType() != ValidPacketTypes.rotation_vector_raw_data_packet) ||
+            (packet.getExpectedLen() != (NUM_ELMTS_IN_QUAT * NUM_BYTES_IN_FLOAT)))
+        {
+            this.setPacketType(ValidPacketTypes.rotation_vector_raw_data_packet);
+            this.setExpectedLen((short) (NUM_ELMTS_IN_QUAT * NUM_BYTES_IN_FLOAT));
+            this.setPayload(new byte[this.getExpectedLen()]);
+        }
+        else
+        {
+        	this.setPacketType(packet.getPacketType());
+        	this.setExpectedLen(packet.getExpectedLen());
+            Serialize(packet.getPayload());
+        }
+    }
+	
+	public void Serialize(byte[] payload) throws Exception
+    {
+        if (payload.length < this.getExpectedLen())
+            throw new Exception();
+
+        this.setPayload(payload);
     }
 	
 	public void Serialize(float[] rawSensorData) throws Exception
@@ -44,4 +75,20 @@ public class Rotation_Vector_RawDataPacket extends DataPacket
         catch (IndexOutOfBoundsException e1) { }
         return rtn;
     }
+	
+	public RotationVector_Quat GetQuat()
+    {
+        RotationVector_Quat rtn = new RotationVector_Quat();
+        float[] vect = DeSerialize();
+        if (vect.length == this.getExpectedLen() / 4)
+        {
+            rtn.x = vect[0];
+            rtn.y = vect[1];
+            rtn.z = vect[2];
+            rtn.w = vect[3];
+        }
+
+        return rtn;
+    }
+
 }
